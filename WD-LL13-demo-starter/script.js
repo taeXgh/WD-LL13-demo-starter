62461c2187069b190abb753f274b29203768fe22
Thalia Edwards <110435033+taeXgh@users.noreply.github.com>
2025-10-22 18:50:53 +0000

    complete

--- WD-LL13-demo-starter/script.js
+++ WD-LL13-demo-starter/script.js
@@ -2,3 +2,17 @@
 // and display it inside the #cat-fact element
 
 // Once that works, we can apply the "pretty-fact" CSS class too
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://catfact.ninja/fact')
+    .then(response => response.json())
+    .then(data => {
+        const factElem = document.getElementById('cat-fact');
+        if (factElem) {
+            factElem.textContent = data.fact;
+            factElem.classList.add('pretty-fact')
+        }
+    })
+    .catch (error=>{
+        console.error('Error fetching cat fact:', error);
+    })
+})
